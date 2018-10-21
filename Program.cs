b20c113ec6311b176b98a30a8e54b02c1d78748a
Krikke21 <christof.lauriers@euri.com>
2018-10-21 15:19:30 +0200

    Test commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AkkaMjrOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! ahahaha");
         }
     }
 }
