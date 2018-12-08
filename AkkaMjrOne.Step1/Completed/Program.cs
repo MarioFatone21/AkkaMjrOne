e87cb5da6eca2b8a67213591db7f01b5cbf3879c
Krikke21 <christof.lauriers@euri.com>
2018-12-08 16:08:06 +0100

    refactor

--- AkkaMjrOne.Step1/Completed/Program.cs
+++ AkkaMjrOne.Step1/Completed/Program.cs
@@ -1,47 +1,47 @@
-﻿using System;
-using Akka.Actor;
+﻿//using System;
+//using Akka.Actor;
 
-namespace AkkaMjrOne.Step1.Completed
-{
-    class Program
-    {
-        // ActorSystem is a heavy object: create only one per application
-        private static ActorSystem MyActorSystem;
+//namespace AkkaMjrOne.Step1.Completed
+//{
+//    class Program
+//    {
+//        // ActorSystem is a heavy object: create only one per application
+//        private static ActorSystem MyActorSystem;
 
-        static void Main(string[] args)
-        {
-            // initialize MyActorSystem
-            MyActorSystem = ActorSystem.Create("MajorOne");
+//        static void Main(string[] args)
+//        {
+//            // initialize MyActorSystem
+//            MyActorSystem = ActorSystem.Create("MajorOne");
 
-            PrintInstructions();
+//            PrintInstructions();
 
-            // time to make your first actors! 
-            // NOTE: use the variant that allows you to pass constructor params for ConsoleReaderActor
-            var consoleWriter = MyActorSystem.ActorOf<ConsoleWriterActor>("consoleWriterActor");
-            var consoleReader = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriter)), "consoleReaderActor");
+//            // time to make your first actors! 
+//            // NOTE: use the variant that allows you to pass constructor params for ConsoleReaderActor
+//            var consoleWriter = MyActorSystem.ActorOf<ConsoleWriterActor>("consoleWriterActor");
+//            var consoleReader = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriter)), "consoleReaderActor");
 
-            // tell console reader to begin
-            consoleReader.Tell("start");
+//            // tell console reader to begin
+//            consoleReader.Tell("start");
 
-            // blocks the main thread from exiting until the actor system is shut down
-            MyActorSystem.WhenTerminated.Wait();
+//            // blocks the main thread from exiting until the actor system is shut down
+//            MyActorSystem.WhenTerminated.Wait();
 
-        }
+//        }
 
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
-    }
-}
+//        private static void PrintInstructions()
+//        {
+//            Console.WriteLine("Write whatever you want into the console!");
+//            Console.Write("Some lines will appear as");
+//            Console.ForegroundColor = ConsoleColor.DarkRed;
+//            Console.Write(" red ");
+//            Console.ResetColor();
+//            Console.Write(" and others will appear as");
+//            Console.ForegroundColor = ConsoleColor.Green;
+//            Console.Write(" green! ");
+//            Console.ResetColor();
+//            Console.WriteLine();
+//            Console.WriteLine();
+//            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+//        }
+//    }
+//}
